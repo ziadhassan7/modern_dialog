71bff6a00c1b890a8e515223db4d14a643cc5512
ziadhassan7 <ziadhassan7@gmail.com>
2023-12-13 16:42:20 +0200

    update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,125 +1,165 @@
 import 'package:flutter/material.dart';
+import 'package:modern_dialog/dialogs/vertical.dart';
+import 'package:modern_dialog/modern_dialog.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      //Theme
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        ),
+
+        //Home
+        home: const HomePage()
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text("Modern Dialog"),
       ),
+
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+
+            ////////////////////////////////////////////////////////////////////
+            // open Standard Dialog
+            ElevatedButton(
+                onPressed: (){
+
+                  /// Standard Dialog
+                  /// normal dialog
+                  ModernDialog.showStandard(
+                    context,
+                    title: "Title",
+                    content: const Text("description"),
+                    buttonTitle: "Save",
+                    onAccept: () => print("do sometime"),
+                  );
+
+                },
+                child: const Text("Standard Dialog")
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            ////////////////////////////////////////////////////////////////////
+
+            const SizedBox(height: 24,),
+
+            ////////////////////////////////////////////////////////////////////
+            // open Info Dialog
+            ElevatedButton(
+                onPressed: (){
+
+                  /// Info Dialog
+                  /// with one agreement button
+                  ModernDialog.showInfo(
+                    context,
+                    content: const Text("You need to know this!"),
+
+                  );
+
+                },
+                child: const Text("Info Dialog",)
             ),
+            ////////////////////////////////////////////////////////////////////
+
+            const SizedBox(height: 24,),
+
+            ////////////////////////////////////////////////////////////////////
+            // open TrailingAction Dialog
+            ElevatedButton(
+                onPressed: (){
+
+                  /// Trailing Action Dialog
+                  /// dialog with two action buttons (main, trailing)
+                  ModernDialog.showTrailingAction(
+                    context,
+                    content: const Text("This a dialog showing a trailing action button"),
+                    buttonTitle: 'Save',
+                    onAccept: () => print("do sometime"),
+
+                    trailingButtonTitle: "Open Settings",
+                    onTrailingPressed: () => print("do sometime"),
+
+                  );
+
+                },
+                child: const Text("Trailing Action Dialog")
+            ),
+            ////////////////////////////////////////////////////////////////////
+
+            const SizedBox(height: 24,),
+
+            ////////////////////////////////////////////////////////////////////
+            // open Custom Dialog
+            ElevatedButton(
+                onPressed: (){
+
+                  /// Vertical Dialog
+                  /// dialog with vertical buttons
+                  ModernDialog.showVerticalDialog(
+                    context,
+                    content: const Text("Here you can add buttons horizontally"),
+                    // your custom buttons
+                    buttons: [
+                      DialogButton(
+                        title: "Discard",
+                        onPressed: () => print("do sometime"),
+                        color: Colors.red,
+                      ),
+
+                      DialogButton(
+                        title: "Allow all changes",
+                        onPressed: () => print("do sometime"),
+                      ),
+                    ],
+                  );
+
+
+                },
+                child: const Text("Vertical Dialog")
+            ),
+            ////////////////////////////////////////////////////////////////////
+
+            const SizedBox(height: 24,),
+
+            ////////////////////////////////////////////////////////////////////
+            // open Custom Dialog
+            ElevatedButton(
+                onPressed: (){
+
+                  /// Custom Dialog
+                  ModernDialog.showCustom(
+                    context,
+                    //replace with your custom widget
+                    view: const Text("Add any view you would like"),
+                  );
+
+                },
+                child: const Text("Custom Dialog")
+            ),
+            ////////////////////////////////////////////////////////////////////
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+
     );
   }
 }
